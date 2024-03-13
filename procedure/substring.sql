2315b928c4a77c67f11904a0838a99880b11348f
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-03-14 00:47:09 +0200

    Update substring.sql

--- procedure/substring.sql
+++ procedure/substring.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SplitSentenceToWordsAndCreateView`(IN input_sentence VARCHAR(255))
 BEGIN
     
-    DECLARE word_separator VARCHAR(1);
+   DECLARE word_separator VARCHAR(1);
     DECLARE start_pos INT;
     DECLARE end_pos INT;
     DECLARE current_word VARCHAR(255);
@@ -10,15 +10,7 @@ BEGIN
     SET word_separator = ' ';
     SET start_pos = 1;
 
-	SET SQL_SAFE_UPDATES = 0;
-
-
-   
-   #SELECT 1 INTO tableExists 
-	#FROM INFORMATION_SCHEMA.TABLES 
-	#WHERE TABLE_SCHEMA = 'project' 
-	#AND TABLE_NAME = 'temp_words';
-
+    SET SQL_SAFE_UPDATES = 0;
 
 	
 	-- Check if the table exists
