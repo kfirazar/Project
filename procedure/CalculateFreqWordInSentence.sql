78ce041173dfbaf3393c380a5ac49ca5605d474e
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-03-31 23:59:10 +0300

    Update CalculateFreqWordInSentence.sql

--- procedure/CalculateFreqWordInSentence.sql
+++ procedure/CalculateFreqWordInSentence.sql
@@ -1,14 +1,15 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CalCulateFreqWordInSentence`()
 BEGIN
+	SET SQL_SAFE_UPDATES = 0;
+
 	Select * from temp_words;
-	INSERT INTO temp_words (Word, Count, Freq)
+	INSERT INTO temp_words (Word, Word_Count, Freq)
 	SELECT Word, COUNT(*) AS WordCount, COUNT(*) / (SELECT COUNT(*) FROM temp_words) AS Frequency
 	FROM temp_words
 	GROUP BY Word;
 	
 	DELETE 
-    	from temp_words
-    	where Count is NULL;
+    from temp_words
+    where Word_Count IS NULL;
 
 END
-CalCulateFreqWordInSentence
