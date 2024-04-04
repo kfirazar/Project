cb6c005d9aec64fabc57dc5b366f60710f4ea579
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-04 12:30:22 +0300

    Update Regression2.sql

--- procedure/Regression2.sql
+++ procedure/Regression2.sql
@@ -1,15 +1,14 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Regression2`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Regression2`(IN username VARCHAR(2505))
 BEGIN
-    #Calcualte the AVG y_hat for each word in the sentence
-    
     DECLARE scope FLOAT;
     DECLARE b FLOAT;
-    DECLARE y_hat FLOAT;
     DECLARE res FLOAT;
     DECLARE x FLOAT;
     DECLARE i INT DEFAULT 1;
     DECLARE done INT DEFAULT FALSE;
-	
+    DECLARE tweetText VARCHAR(2505);
+    DECLARE sum float;
+
     -- Declare cursor
     DECLARE rowCur CURSOR FOR SELECT GeneralFreq FROM temp_words;
 	
@@ -19,6 +18,9 @@ BEGIN
     -- Set SQL_SAFE_UPDATES
     SET SQL_SAFE_UPDATES = 0;
 
+    -- Get the latest tweet text
+    SET tweetText = (SELECT Tweet FROM tweets ORDER BY ID DESC LIMIT 1);
+    
     -- Calculate scope
     SELECT SUM(AvgGeneralFreqDiff * AvgRealFreqDiff) / SUM(AvgGeneralFreqDiff * AvgGeneralFreqDiff) INTO scope
     FROM dataforregression;
@@ -26,22 +28,20 @@ BEGIN
     -- Calculate b
     SELECT SUM(AvgGeneralFreqDiff) - SUM(AvgRealFreqDiff) * scope INTO b
     FROM dataforregression;
-	select scope ,b;
-
+	
     -- Open cursor
     OPEN rowCur;
 	
-    set res = 0;
+    SET res = 0;
     -- Start loop
     read_loop: LOOP
         FETCH rowCur INTO x;
-		
         IF done THEN
             LEAVE read_loop;
         END IF;
 	
         -- Calculate y_hat for each row and accumulate the result
-        if x = NULL then 
+        IF x IS NULL THEN 
 			SET x = 0;
         END IF;
         
@@ -52,7 +52,21 @@ BEGIN
     -- Close cursor
     CLOSE rowCur;
 
-    -- Calculate the average y_hat and output the result
-    SELECT 100*res / i  AS "y hat --> The engagement of the word based on frequency to engagement";
+    -- Calculate the average y_hat
+    SELECT 100 * res / i  INTO sum
+    FROM DUAL;
 
+    -- Update the prediction of the tweet
+    UPDATE tweets 
+    SET PureEngage = sum
+    WHERE Tweet = tweetText AND PureEngage IS NULL;
+
+    -- Update the history of AVG prediction engagement of the user
+    UPDATE engage_data
+    SET Avg_Engagement = (
+        SELECT COALESCE(AVG(PureEngage), 0)
+        FROM tweets 
+        WHERE UserId = 14
+    )
+    WHERE User_name = username;
 END
