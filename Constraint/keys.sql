1f08601bf2bf252a655d85b86b30aa33444be11d
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-05 02:19:46 +0300

    Update keys.sql

--- Constraint/keys.sql
+++ Constraint/keys.sql
@@ -11,9 +11,20 @@ REFERENCES tweets(UserId);
 
 
 
-alter table comments
-ADD CONSTRAINT key7 FOREIGN KEY (tweetId)
-REFERENCES tweets(Id);
+
+-- Change the columns and add primary key
+ALTER TABLE comments
+MODIFY COLUMN tweetId INT NOT NULL,
+MODIFY COLUMN commentId INT NOT NULL AUTO_INCREMENT,
+ADD PRIMARY KEY (commentId, tweetId);
+
+-- Add the new foreign key constraint
+ALTER TABLE comments
+ADD CONSTRAINT key7 FOREIGN KEY (tweetId) REFERENCES tweets(Id);
+
+
+ALTER TABLE word_in_tweet
+DROP FOREIGN KEY key5;
 
 ALTER TABLE word_in_tweet
 ADD CONSTRAINT key5 FOREIGN KEY (tweetId)
@@ -22,3 +33,4 @@ REFERENCES tweets(Id);
 ALTER TABLE word_in_tweet
 ADD CONSTRAINT key6 FOREIGN KEY (wordId)
 REFERENCES freq_of_words(wordId);
+
