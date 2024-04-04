01936ee955064115d54b7ebd49369706cb87dc79
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-05 01:48:14 +0300

    Update keys.sql

--- Constraint/keys.sql
+++ Constraint/keys.sql
@@ -3,13 +3,22 @@ CREATE INDEX idx_tweets_userId ON tweets(UserId);
 CREATE INDEX idx_dataforregression_GeneralFreq ON dataforregression(GeneralFreq);
 
 
-ALTER TABLE freq_of_words
-ADD CONSTRAINT key3
-FOREIGN KEY (AvgEng)
-REFERENCES dataforregression(GeneralFreq);
-
 
 ALTER TABLE engage_data
 ADD CONSTRAINT key1
 FOREIGN KEY (User_num)
 REFERENCES tweets(UserId);
+
+
+
+alter table comments
+ADD CONSTRAINT key7 FOREIGN KEY (tweetId)
+REFERENCES tweets(Id);
+
+ALTER TABLE word_in_tweet
+ADD CONSTRAINT key5 FOREIGN KEY (tweetId)
+REFERENCES tweets(Id);
+
+ALTER TABLE word_in_tweet
+ADD CONSTRAINT key6 FOREIGN KEY (wordId)
+REFERENCES freq_of_words(wordId);
