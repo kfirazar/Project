680e13d210e7817ebc06ab0cc30599e2eef20a65
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-05 16:50:08 +0300

    Update GeneralFreqOfWord.sql

--- procedure/GeneralFreqOfWord.sql
+++ procedure/GeneralFreqOfWord.sql
@@ -4,11 +4,11 @@ BEGIN
 	    DECLARE general_amount INT;
 	    SELECT SUM(Word_Count) INTO general_amount  FROM  freq_of_words;
 
-	    --Update the the freq of each word from "freq_of_words" table
+	    -- Update the the freq of each word from "freq_of_words" table
 	    UPDATE freq_of_words fow SET AvgEng = fow.Word_Count / general_amount;
 
 
-            --Update the value for the freq inside "temp_words" table
+		-- Update the value for the freq inside "temp_words" table
 	    UPDATE temp_words fow
 	    SET GeneralFreq = (SELECT AvgEng  from freq_of_words a1 where  a1.Word = fow.Word);
 						
