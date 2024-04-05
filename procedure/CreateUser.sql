1d179cbd3e4cb752fdc4e1bc9799cd70b20c2323
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-05 16:50:59 +0300

    Update CreateUser.sql

--- procedure/CreateUser.sql
+++ procedure/CreateUser.sql
@@ -4,5 +4,7 @@ BEGIN
     if (select COUNT(*) from engage_data where User_name = username) = 0 THEN
 		INSERT engage_data (Amount_of_followers,Avg_Engagement,User_name)
         VALUES(followers,0,username);
+        
+        
 	end if;
 END
