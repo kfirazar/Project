a40ea0aa10672bb531f3fcf8a5d8780c8747ce46
Kfir Shimonov Azar <111119507+kfirazar@users.noreply.github.com>
2024-04-05 16:49:17 +0300

    Update InsertTweet.sql

--- procedure/InsertTweet.sql
+++ procedure/InsertTweet.sql
@@ -1,12 +1,12 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertTweet`(IN Username VARCHAR(255), IN Engagement FLOAT, IN input_sentence VARCHAR(255))
 BEGIN
 	
-    DECLARE excel_serial_date INT;
-    SET excel_serial_date = DATEDIFF(CURRENT_DATE(), '1900-01-01') + 1;
+    DECLARE current_date_formatted DATE;
+    SET current_date_formatted = CURRENT_DATE();
 	
     if (select COUNT(*) from engage_data where User_name = Username) = 1 THEN
 		INSERT INTO tweets (UserId, Tweet, DateOfUpload, Engagement)
-		VALUES ((SELECT User_num FROM engage_data WHERE User_name = Username), input_sentence, excel_serial_date, Engagement);
+		VALUES ((SELECT User_num FROM engage_data WHERE User_name = Username), input_sentence, current_date_formatted, Engagement);
 	
     
 		CALL SplitSentenceToWordsAndCreateView(input_sentence);
